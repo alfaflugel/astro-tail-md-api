c344f18f41a3e9cb3a771960d58f7068b61f57c8
Frederikke <frfl0001@stud.kea.dk>
2024-04-23 11:37:34 +0200

    opgave

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'persianmosaic': {
-                    500: '#196673',
-                  },
-            }
-        }
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Prompt"', ...defaultTheme.fontFamily.sans],
+
+        display: ['"Roboto"', "monospace"], // Adds a new `font-display` class
+      },
+
+      colors: {
+        persianmosaic: {
+          100: "#d2dfe2",
+          200: "#90b0b7",
+          300: "#64929b",
+          400: "#4e838e",
+          500: "#196673",
+          600: "#145863",
+          700: "#104a54",
+          800: "#072f36",
+          900: "#042328",
+        },
+      },
     },
-  };
+  },
+};
